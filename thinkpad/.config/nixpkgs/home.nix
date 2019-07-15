6e490d86b1a225d5624a6e689f8f3fd170bbb30b
Oscar Duignan <oscar@mutualism.co.uk>
2019-07-15 20:01:59 +0100

    Correct the way I was building notational-fzf-vim

--- thinkpad/.config/nixpkgs/home.nix
+++ thinkpad/.config/nixpkgs/home.nix
@@ -5,20 +5,32 @@ let
 
   vim-which-key = buildVimPlugin {
     name = "vim-which-key";
+
     src = builtins.fetchGit {
       url = "https://github.com/liuchengxu/vim-which-key.git";
       rev = "ccb656f30ccafc00cf633fa4b42540739f538356";
     };
+
     dependencies = [];
   };
 
   notational-fzf-vim = buildVimPlugin {
     name = "notational-fzf-vim";
+
     src = builtins.fetchGit {
-      url = "https://github.com/oscarduignan/notational-fzf-vim.git";
-      ref = "patch-1";
-      rev = "ef25865a7d4beb4d08812bcfbd77714e8fb08e25";
+      url = "https://github.com/alok/notational-fzf-vim.git";
+      rev = "9daf78b4bd7c993b1065579bcb8a26b83c1681c9";
     };
+
+    preFixup = ''
+      substituteInPlace $out/share/vim-plugins/notational-fzf-vim/plugin/notational_fzf.vim \
+        --replace "'rg'" "'${pkgs.ripgrep}/bin/rg'" \
+        --replace "'python3'" "'${pkgs.python3}/bin/python'" \
+        --replace "if !exists('g:nv_search_paths')" \
+                  "let g:nv_search_paths = get(g:, 'nv_search_paths', ['./Notes'])
+                   if !exists('g:nv_search_paths')"
+    '';
+
     dependencies = [];
   };
 
@@ -72,8 +84,6 @@ in {
         let g:maplocalleader = ','
         nnoremap <silent> <leader>      :<c-u>WhichKey '<Space>'<CR>
         nnoremap <silent> <localleader> :<c-u>WhichKey  ','<CR>
-
-        let g:nv_search_paths = ['~/Notebooks', './docs']
 
         let g:which_key_map = {}
 
